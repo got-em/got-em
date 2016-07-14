8131d9ed75674dd807cbb35b13f9dffc72f3b99e
David Menendez <dmenend@us.ibm.com>
2016-07-14 15:25:33 -0500

    implemented basic addon functionality

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var sounds = require('../lib/sounds');
-var soundsGroup = [];
-var groupSize = 4;
-for(var i = 0; i < sounds.length; i += groupSize){
-  soundsGroup.push(sounds.slice(i, i + groupSize));
-}
+var getSounds = require('../lib/sounds');
 
 module.exports = io => {
   router.get('/', function(req, res, next) {
@@ -13,6 +8,18 @@ module.exports = io => {
   });
 
   router.get('/room/:room_id', function(req, res, next) {
+    var addon = req.query.addon;
+    var sounds = getSounds(addon);
+
+    io.on('connection', function(socket) {
+      socket.emit('load', sounds);
+    });
+
+    var soundsGroup = [];
+    var groupSize = 4;
+    for(var i = 0; i < sounds.length; i += groupSize){
+      soundsGroup.push(sounds.slice(i, i + groupSize));
+    }
     res.render('room', { soundsGroup });
   });
 
